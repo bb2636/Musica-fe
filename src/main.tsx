c5581db42f5953620e12ca41180457ae37beb1c8
hhs5486 <hhs5486@naver.com>
2025-07-18 14:26:56 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-import './/src/styles/global.css' // 여기에 연결
+import '../src/styles/global.css' // 여기에 연결
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- src/pages/Mypage/INSTRUCTOR/InstructorMyPage.tsx
+++ src/pages/Mypage/INSTRUCTOR/InstructorMyPage.tsx
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 // pages/Mypage/INSTRUCTOR/InstructorMyPage.tsx
 import { Outlet } from 'react-router-dom';
 import InstructorLayout from '../../../components/layout/InstructorLayout';
@@ -12,17 +12,3 @@ const InstructorMyPage = () => {
 };
 
 export default InstructorMyPage;
-=======
-import React from 'react';
-import InstructorRevenueStats from '../../../components/InstructorRevenueStats';
-
-export default function InstructorMyPage() {
-    return (
-        <div className="max-w-3xl mx-auto w-full px-4 py-10">
-            <h1 className="text-2xl font-bold mb-8">강사 마이페이지</h1>
-
-            <InstructorRevenueStats />
-        </div>
-    );
-}
->>>>>>> Stashed changes
